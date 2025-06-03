8be26ae7d9da9218dd326e15e057201dd12baa1b
samuel <samshimoni@gmail.com>
2025-06-03 15:07:29 +0300

    new main.cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,20 @@
 
 #include "ProcessChecker/ProcessChecker.hpp"
 #include "ProcessExecutor/ProcessExecutor.hpp"
+
 #include "influxdb-cpp/influxdb.hpp"
 
 
-int main() {    
-    influxdb_cpp::server_info si("localhost", 8086, "states");
+void show_db(){
+    influxdb_cpp::server_info si("192.168.1.110", 8086, "states");
+
+    std::string resp;
+    influxdb_cpp::query(resp, "select * from process_status", si);
+    std::cout << resp << std::endl;
+}
+
+int main() {
+    influxdb_cpp::server_info si("192.168.1.110", 8086, "states");
 
     std::vector<std::string> names = {
         "pythonsolar_monitor_grafana.py",
@@ -21,19 +30,22 @@ int main() {
     ProcessChecker checker;
     ProcessExecutor executor(checker, ProcessChecker::BucketsTypes::COMMUNICATION);
 
+
     while (true) {
         std::vector<std::string> procesess_to_execute = {};
+
         auto results = checker.checkProcesses(names);
+
         for (const auto& [name, running] : results) {
             if (running == 0){
                 procesess_to_execute.push_back(name);
             }
         }
 
-        if (!procesess_to_execute.empty()){
-            checker.Notify(ProcessChecker::BucketsTypes::COMMUNICATION, procesess_to_execute);
+        if (!procesess_to_execute.empty()) {
+               checker.Notify(ProcessChecker::BucketsTypes::COMMUNICATION, procesess_to_execute);    
         }
-
+        
         influxdb_cpp::builder()
         .meas("process_status")
         .field("solar", results["pythonsolar_monitor_grafana.py"] )
@@ -41,7 +53,7 @@ int main() {
         .field("temperatur_sensor", results["pythonmeshtastic_recv_handler.py"])
         .post_http(si);
 
-        std::this_thread::sleep_for(std::chrono::minutes(2));
+        std::this_thread::sleep_for(std::chrono::minutes(5));
         std::cout<<std::endl;
     }
 
